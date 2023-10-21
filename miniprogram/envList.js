99154e3add6fa41cf8af7d2dc1944432f07a16a9
Azur冰弦 <973562770@qq.com>
2023-10-21 21:49:37 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"demo-2g5tgbq79a580cda","alias":"demo"}]
+const isMac = true
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.sh
@@ -0,0 +1,1 @@
+"/Applications/wechatwebdevtools.app/Contents/MacOS/cli" cloud functions deploy --e demo-2g5tgbq79a580cda --n quickstartFunctions --r --project "/Users/azurice/Dev/YuChuan/demo" --report_first --report
